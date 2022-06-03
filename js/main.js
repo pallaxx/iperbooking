1aedb147f8794ebfbd1e31a7dd991ea72ccdedf5
pallaxx <alexmaz03@hotmail.it>
2022-06-03 21:06:09 +0200

    3 June

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ var nome, email, profilepic; //page user
 var pagenumber=1,maxpages; //page prenotazioni
 const arraygiorni = ["gennaio","febbraio","marzo","aprile","maggio","giugno","luglio","agosto","settembre","ottobre","novembre","dicembre"];
 
-
 function serviceWorkerRegister() {
   'use strict';
   
@@ -43,190 +42,15 @@ function findCrossCookies() {
         email = json.email;
         profilepic = "https://iperbooking.cristiano/"+json.foto;
         profilepic = "https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip";//perchè le immagini risiedono su protocollo http
-        if(location.href=="https://site.test/MobileApp/login.html")
+        if(!location.pathname.includes("index.html"))
           location.href="index.html";
       }
       else
       {   //  PER FARE LA GRAFICA DISABILITARE
-        if(location.href!="https://site.test/MobileApp/login.html")
+        if(!location.pathname.includes("login.html")) //https://site.test/login.html oppure https://site.test/MobileApp/login.html
         location.href="login.html";
       }
     } catch(error){
     }
   }
-}
-//       credenziali di prova
-// email: alex.mazzoni17@gmail.com
-// pdw: Test123456
-
-
-
-// ------ prenotazione page --------------------------------------------------------------------------------------------------------------
-//AJAX per le prenotazioni
-function showFilter() {
-  
-}
-
-function ElencoPrenotazioni() {
-    //suddivisione di ogni filtro
-    let codice = document.getElementById("prenotazioni_search").value; //filtra per codice o nome (un po di tutto)
-    let canale = "0"; // se 0 prende tutto (esempio di canali. (Agoda, Adriatico, CRM, Booking.com))
-    let data_partenza = ""; //data di partenza
-    let data_arrivo = ""; //data di arrivo
-    let  filtradataper = ""; //prenotazione, partenza, arrivo, presenza
-    var url = site+"/ajax/proxy.cfm?action=prenotazione.cerca&codice="+codice+"&canale="+canale+"&dal="+data_partenza+"&al="+data_arrivo+"&filtroData="+filtradataper+"&page="+pagenumber+"&pageSize=10&sort%5B0%5D%5Bfield%5D=id&sort%5B0%5D%5Bdir%5D=desc&_=1653313538581";
-    let xhrfindcookies = new XMLHttpRequest();
-    xhrfindcookies.withCredentials = true;
-    xhrfindcookies.open("GET", url, true);
-    xhrfindcookies.send();
-    xhrfindcookies.onload = handleElencoPrenotazioniResponse; 
-} function handleElencoPrenotazioniResponse() {
-  if(this.status==200)
-  {
-    try { //costruire la tabella
-      let json = JSON.parse(this.responseText);
-      maxpages = json.total;
-      var row="";
-      for (let index = 0; index < json.count; index++) {
-        row += '< class="prenotazione" onclick="RedirectPrenotazione('+json.data[index].id+'">'; //CLICCANDO IN QUELLA PRENOTAZIONE RICHIEDE TRAMITE L'ID LA PRNOTAZIONE E STAMPA UNA PAGINA
-         row += '<div class="dataprenotazione">'+ stampaData(json.data[index].dataPrenotazione) + ' ' + json.data[index].orarioPrenotazione +'</div>'; //DATA DI PRENOTAZIONE
-         row += '<div class="cliente">'+json.data[index].cliente+'</div>'; //NOME COGNOME CLIENTE
-         row += '<div class="canale">'+json.data[index].canale+'</div>';   //CANALE DELLA PRENOTAZIONE
-         row += '<div class="stagione_periodo">'; // STAGIONE E PERIODO
-           row += '<div class="stagione-group">'+stampaGiorno(json.data[index].dataPrenotazione)+'</div>';
-           row += '<div class="periodo-group">' + stampaDataNoAnno(json.data[index].partenza) +' - '+ stampaDataNoAnno(json.data[index].arrivo) + '</div>';
-         row += '</div>';
-         row += '<div class="camere_notti">'; // CAMERE E NOTTI
-          row += '<div class="camere-group">'; // CAMERE
-            row += '<div class="icona"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/><path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></svg></div>'; 
-            row += '<div class="testo">'+json.data[index].quantita.slice(0, 1)+'</div>'; 
-          row += '</div>'; 
-          row += '<div class="notti-group">'; // NOTTI
-            row += '<div class="icona"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/><path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></svg></div>'; 
-            row += '<div class="testo">'+json.data[index].notti+'</div>'; 
-          row += '</div>'; 
-        row += '</div>';
-      }
-
-        // INDICE PER SCORRERE LE PAGINE
-      row += '<div class="indice">';
-        if(pagenumber==1)
-          row += '<div class="arrow-left">';
-        else
-          row += '<div class="arrow-left" onclick="previousPage()">';
-          row += '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg>'; 
-        row += '</div>'; 
-
-        row += '<div class="number">'+pagenumber+'</div>';
-        if(pagenumber==maxpages)
-          row += '<div class="arrow-right">';
-        else
-          row += '<div class="arrow-right" onclick="nextPage()">';
-        row += '<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg>'; 
-        row += '</div>'; 
-      row += '</div>'; 
-      
-      document.getElementById("elenco").innerHTML = row;
-    
-    }catch(error) {}
-  }
-}
-function RedirectPrenotazione(codice) {
-  
-}
-function nextPage() {
-  pagenumber++;
-  document.getElementById("elenco").innerHTML = '';
-  ElencoPrenotazioni();
-}
-function previousPage() {
-  pagenumber--;
-  document.getElementById("elenco").innerHTML = '';
-  ElencoPrenotazioni();
-}
-
-//20220525 anno mese giorno
-function stampaData(stringadata) {return stringadata.slice(6, 8)+'/'+stringadata.slice(4, 6)+'/'+stringadata.slice(2, 4);} //stampa 23/01/2022
-function stampaDataNoAnno(stringadata) {return stringadata.slice(6, 9)+'/'+stringadata.slice(4, 6)} //stampa 23/01
-function stampaGiorno(stringadata) {return arraygiorni[parseInt(stringadata.slice(4, 6))+1]} //stampa tipo gennaio dato 01 (stringa)
-
-
-
-// ------ user page --------------------------------------------------------------------------------------------------------------
-//copia l'email toccandola
-function copy(field) { navigator.clipboard.writeText(field); }
-
-//AJAX per sloggare
-function Logout() {
-  var url = site+"?event=logout";
-  let xhrfindcookies = new XMLHttpRequest();
-  xhrfindcookies.withCredentials = true;
-  xhrfindcookies.open("GET", url, true);
-  xhrfindcookies.send();
-  xhrfindcookies.onload = handleLogoutResponse;
-} function handleLogoutResponse() {
-  if(this.status==200)
-    findCrossCookies();
-}
-
-
-
-// ------ show/hide pages --------------------------------------------------------------------------------------------------------------
-viewHome(); //appena lO script viene caricato la pagina mostra la home
-
-function viewHome() { //pagona centrale
-  clearPages();
-  document.getElementById("navbar_home").classList.add("active");
-  document.getElementById("home").style.display = '';
-}
-function viewUser() { //pagina che si apre cliccando l'icona (nella home)
-  clearPages();
-  document.getElementById("user").classList.add("active");
-  document.getElementById("user").style.display = '';
-
-  // Update
-  document.getElementById("bigprofilepic").src = profilepic;
-  document.getElementById("user_nome").innerHTML = nome;
-  document.getElementById("user_email").value = email;
-  document.getElementById("user_sites").innerHTML = "<a class='h4' href='https://backoffice.iperbooking.cristiano/'>backoffice.iperbooking.cristiano</a><br><a class='h4' href='https://www.google.com'>GOOGLE</a>";
-}
-
-function viewPrenotazioni() { //pagina tutta a sinistra (la chiave)
-  clearPages();
-  document.getElementById("navbar_prenotazioni").classList.add("active");
-  document.getElementById("prenotazioni").style.display = '';
-  ElencoPrenotazioni();
-}
-function viewCalendar() { //pagina tutta a sinistra (la chiave)
-  clearPages();
-  document.getElementById("navbar_calendar").classList.add("active");
-  document.getElementById("calendar").style.display = '';
-}
-function viewApple() { //pagina tutta a sinistra (la chiave)
-  clearPages();
-  document.getElementById("navbar_apple").classList.add("active");
-  document.getElementById("apple").style.display = '';
-}
-function viewSettings() { //pagina tutta a sinistra (la chiave)
-  clearPages();
-  document.getElementById("navbar_settings").classList.add("active");
-  document.getElementById("settings").style.display = '';
-}
-
-function clearPages() {
-  document.getElementById("home").style.display = 'none';
-  document.getElementById("navbar_home").classList.remove("active");
-  document.getElementById("user").style.display = 'none';
-
-  document.getElementById("prenotazioni").style.display = 'none';
-  document.getElementById("navbar_prenotazioni").classList.remove("active");
-
-  document.getElementById("calendar").style.display = 'none';
-  document.getElementById("navbar_calendar").classList.remove("active");
-
-  document.getElementById("apple").style.display = 'none';
-  document.getElementById("navbar_apple").classList.remove("active");
-
-  document.getElementById("settings").style.display = 'none';
-  document.getElementById("navbar_settings").classList.remove("active");
 }
